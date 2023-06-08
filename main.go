4b5db2a4af61ad1f8461ea62d42b2c684b15352e
Vyacheslav Zamaraev <zamaraev@gmail.com>
2023-06-08 13:01:47 +0500

    make deleting old stat files

--- main.go
+++ main.go
@@ -2,65 +2,114 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"path"
 	"path/filepath"
+	"regexp"
 )
 
 func main() {
-	// perl "C:/Apps/awstats/wwwroot/cgi-bin/awstats.pl" -config="arcgis" -update -LogFile="C:\inetpub\logs\LogFiles\W3SVC1\u_ex220101.log"
+	// perl "C:/Apps/awstats/wwwroot/cgi-bin/awstats.pl" -config="configName" -update -LogFile="C:\inetpub\logs\LogFiles\W3SVC1\u_ex220101.log"
 
 	var log_folder string = "C:\\inetpub\\logs\\LogFiles\\W3SVC1\\"
+	var path_cgi_bin string = "C:\\Apps\\awstats\\wwwroot\\cgi-bin\\"
+	var config_name string = "arcgis"
 
 	fileName := "update_ststistics.bat"
 
 	f, err := os.Create(fileName)
 
 	if err != nil {
+		fmt.Println("Unable to create file:", err)
 		log.Fatal(err)
+		os.Exit(1)
 	}
 
 	defer f.Close()
 
-	fmt.Println("List files by Walk")
-	listDirByWalk(log_folder, f)
+	deleteOldStatistics(path_cgi_bin, config_name, f)
+	fmt.Println("List files: ")
+	listDirByWalk(log_folder, f, path_cgi_bin, config_name)
 
 }
 
-func listDirByWalk(path string, f *os.File) {
+func deleteOldStatistics(path string, config_name string, f *os.File) {
+	// var files []string
+	// files, err := getFilesStatistics(path, ext_awstats)
+
+	var ext_awstats string = "." + config_name + ".txt"
+	var files []string
+	fmt.Println("Start deleting")
+
+	files, err := listDirByIOReadDir(path, ext_awstats)
+
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		fmt.Println(file)
+		var str_bat_cmd string = "del \"" + file + "\""
+		f.WriteString(str_bat_cmd + "\n")
+
+		// fmt.Printf("[%s]\n", path)
+	}
+
+}
+
+func listDirByIOReadDir(root string, ext string) ([]string, error) {
+	var files []string
+	// var err2 error
+	fileInfo, err := ioutil.ReadDir(root)
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+		return files, err
+	}
+	for _, file := range fileInfo {
+		reg, err2 := regexp.Compile(ext)
+
+		if err2 != nil {
+			fmt.Printf("Error: %v", err2)
+			return nil, err2
+		}
+
+		if reg.MatchString(file.Name()) {
+			full_path := path.Join(root, file.Name())
+			files = append(files, full_path)
+		}
+	}
+	return files, nil
+}
+
+func listDirByWalk(path string, f *os.File, path_cgi_bin string, config_name string) {
 	filepath.Walk(path, func(wPath string, info os.FileInfo, err error) error {
 
 		var perl_name string = "perl"
-		var file_script_path string = "\"C:/Apps/awstats/wwwroot/cgi-bin/awstats.pl\""
-		var config_name string = "-config=\"arcgis\""
-		var parametr_name string = "-update"
+		// var path_cgi_bin string = "C:/Apps/awstats/wwwroot/cgi-bin/"
+		var path_file_script string = "\"" + path_cgi_bin + "awstats.pl\""
+		var param_config string = "-config=\"" + config_name + "\""
+		var param_name string = "-update"
+		var param_logfile string = "-LogFile="
 
-		// Обход директории без вывода
-		// if wPath == path {
-		// 	return nil
-		// }
-
-		// Если данный путь является директорией, то останавливаем рекурсивный обход
-		// и возвращаем название папки
-		// if info.IsDir() {
-		// 	fmt.Printf("[%s]\n", wPath)
-		// 	log.Println(wPath)
-		// 	return filepath.SkipDir
-		// }
-
-		// Выводится название файла
+		// Выводится команда
 		if wPath != path {
-			// fmt.Println(wPath)
-			var qq string = perl_name + " " + file_script_path + " " + config_name + " " + parametr_name + " " + "-LogFile=\"" + wPath + "\""
-			fmt.Println(qq)
+			var qq string = perl_name + " " + path_file_script + " " + param_config + " " + param_name + " " + param_logfile + "\"" + wPath + "\""
 			f.WriteString(qq + "\n")
-
-			// fmt.Printf("[%s]\n", info.Name())
-			// fmt.Printf("[%d]\n", info.Size())
-			// fmt.Printf("[%d]\n", info.ModTime().Day())
-			// log.Println(wPath)
-			// fmt.Printf("%s %v %v %v\n", perl_name, file_script_path, config_name, parametr_name)
 		}
 		return nil
 	})
 }
+
+// func FilePathWalkDir(root string) ([]string, error) {
+// 	var files []string
+// 	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+// 		if !info.IsDir() {
+// 			files = append(files, path)
+// 		}
+// 		return nil
+// 	})
+// 	return files, err
+// }
